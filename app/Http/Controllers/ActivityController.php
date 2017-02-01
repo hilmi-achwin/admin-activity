87595e1e281589328eac794555abfc3b4da6182d
Hilmi <hilmi@olimpiade.id>
2017-02-01 16:25:02 +0700

    add

--- app/Http/Controllers/ActivityController.php
+++ app/Http/Controllers/ActivityController.php
@@ -20,7 +20,7 @@ use PDF;
 use Illuminate\Mail\Mailer;
 use Illuminate\Mail\Message;
 use Carbon\Carbon;
-
+include_once(app_path().'/phpmailer/class.phpmailer.php');
 
 class ActivityController extends Controller
 {   
@@ -68,59 +68,69 @@ class ActivityController extends Controller
        
       
        
-        $waktu_selesai = Carbon::now(7);
-        $modul_terpasang = implode(',', $request['modul_terpasang']);
-        $precheckInput = Precheck::create([
-            'id_activity' => $request->input('id_activity'),
-            'fisik' => $request->input('fisik'),
-            'os' => $request->input('os'),
-            'modul_terpasang' => $request->input('modul_terpasang'),
-            'modem' => $request->input('modem'),
-            'accessories' => $request->input('accessories'),
-            'num_dial_iplus' => $request->input('num_dial_iplus'),
-            'ip_extreme' => $request->input('ip_extreme'),
-            'directory_modul' => $request->input('directory_modul'),
-            'lain-lain' => $request->input('lain-lain'),
-            'modul_terpasang' => $modul_terpasang
-            ]);
+        // $waktu_selesai = Carbon::now(7);
+        // $modul_terpasang = implode(',', $request['modul_terpasang']);
+        // $precheckInput = Precheck::create([
+        //     'id_activity' => $request->input('id_activity'),
+        //     'fisik' => $request->input('fisik'),
+        //     'os' => $request->input('os'),
+        //     'modul_terpasang' => $request->input('modul_terpasang'),
+        //     'modem' => $request->input('modem'),
+        //     'accessories' => $request->input('accessories'),
+        //     'num_dial_iplus' => $request->input('num_dial_iplus'),
+        //     'ip_extreme' => $request->input('ip_extreme'),
+        //     'directory_modul' => $request->input('directory_modul'),
+        //     'lain-lain' => $request->input('lain-lain'),
+        //     'modul_terpasang' => $modul_terpasang
+        //     ]);
 
 
-        $postcheckInput = Postcheck::create([
-            'id_activity' => $request->input('id_activity'),
-            'pengembalian_fungsi_awal' => $request->input('pengembalian_fungsi_awal'),
-            'fungsi_model_edii' => $request->input('fungsi_model_edii'),
-            'fungsi_pembentukan_edifact' => $request->input('fungsi_pembentukan_edifact'),
-            'fungsi_komunikasi' => $request->input('fungsi_komunikasi'),
-            'penyelesaian_semua_keluhan' => $request->input('penyelesaian_semua_keluhan'),
-            'serah_terima' => $request->input('serah_terima'),
-            'lain-lain' => $request->input('lain-lain'),
-            ]);
+        // $postcheckInput = Postcheck::create([
+        //     'id_activity' => $request->input('id_activity'),
+        //     'pengembalian_fungsi_awal' => $request->input('pengembalian_fungsi_awal'),
+        //     'fungsi_model_edii' => $request->input('fungsi_model_edii'),
+        //     'fungsi_pembentukan_edifact' => $request->input('fungsi_pembentukan_edifact'),
+        //     'fungsi_komunikasi' => $request->input('fungsi_komunikasi'),
+        //     'penyelesaian_semua_keluhan' => $request->input('penyelesaian_semua_keluhan'),
+        //     'serah_terima' => $request->input('serah_terima'),
+        //     'lain-lain' => $request->input('lain-lain'),
+        //     ]);
 
-        $jenis_perusahaan = implode(',', $request['jenis_perusahaan']);
-          $perusahaan = Perusahaan::where('nama_perusahaan',$request->input('nama_perusahaan'))->
-            update([
-            'nama_perusahaan'   => $request->input('nama_perusahaan'),
-            'alamat'            => $request->input('alamat'),
-            'fax'               => $request->input('fax'),
-            'nomor_telepon'        => $request->input('nomor_telepon'),
-            'no_hp'             => $request->input('no_hp'),
-            'email'             => $request->input('email'),
-            'kontak_person'     => $request->input('kontak_person'),
-            'npwp'              => $request->input('npwp'),
-            'edi_number'        => $request->input('edi_number'),
-            'enabler'           => $request->input('enabler'),
-            'jenis_perusahaan'  => $jenis_perusahaan
-            ]);
+        // $jenis_perusahaan = implode(',', $request['jenis_perusahaan']);
+        //   $perusahaan = Perusahaan::where('nama_perusahaan',$request->input('nama_perusahaan'))->
+        //     update([
+        //     'nama_perusahaan'   => $request->input('nama_perusahaan'),
+        //     'alamat'            => $request->input('alamat'),
+        //     'fax'               => $request->input('fax'),
+        //     'nomor_telepon'        => $request->input('nomor_telepon'),
+        //     'no_hp'             => $request->input('no_hp'),
+        //     'email'             => $request->input('email'),
+        //     'kontak_person'     => $request->input('kontak_person'),
+        //     'npwp'              => $request->input('npwp'),
+        //     'edi_number'        => $request->input('edi_number'),
+        //     'enabler'           => $request->input('enabler'),
+        //     'jenis_perusahaan'  => $jenis_perusahaan
+        //     ]);
 
-        $activityUpdate = Activity::where('id', $request->input('id_activity'))->update([
-            'tindakan_perbaikan' => $request->input('tindakan_perbaikan'),
-            'nama_admin' => Auth::User()->id,
-            'status' => 'Sudah',
-            'waktu_selesai' => $waktu_selesai->toTimeString()
-            ]);
+        // $activityUpdate = Activity::where('id', $request->input('id_activity'))->update([
+        //     'tindakan_perbaikan' => $request->input('tindakan_perbaikan'),
+        //     'nama_admin' => Auth::User()->id,
+        //     'status' => 'Sudah',
+        //     'waktu_selesai' => $waktu_selesai->toTimeString()
+        //     ]);
     
                 
-  
+   $mail = new \PHPMailer(true);
+        $body = 'An email test!';
+ 
+$mail->AddReplyTo('hilmiweq321@gmail.com', 'Clark Kent');
+$mail->SetFrom('hilmiweq321@gmail.com', 'Clark Kent');
+$mail->AddAddress('hilmiweq321@gmail.com', 'Code Chewing');
+$mail->Subject = 'Test email';
+$mail->MsgHTML( "weq");
+ 
+if( ! $mail->Send() ) {
+  echo "Mailer Error: " . $mail->ErrorInfo;
 
         // $message = sprintf('Klik link berikut untuk aktivasi akun Hai Unair anda ');
 
@@ -132,4 +142,4 @@ class ActivityController extends Controller
 
    
 
-}
+}}
